ac0d15c4f86928bc4b8a53b0b73b0a2c85cf1302
Rafee Jenkins <rafeejenkins@gmail.com>
2018-11-15 11:59:46 +0000

    Render the test Tiled file (teststage.tmx).

--- core/src/com/mygdx/zombies/states/Stage.java
+++ core/src/com/mygdx/zombies/states/Stage.java
@@ -6,40 +6,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.zombies.Entity;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class Stage extends State {
 	
-	private ArrayList<Entity> clippingList;
-	private ArrayList<Entity> nonClippingList;
+	private TiledMap map;
+	private String mapFile;
+	private OrthogonalTiledMapRenderer renderer;
+	private OrthographicCamera camera;
 	
-
 	/**Constructor for the stage
-	 * @param path - filename of the stage layout
+	 * @param path - name of .tmx file for tiled grid
 	 */
 	public Stage(String path) {
 		super();
-			
-		HashMap<Character, Texture> dictionary = new HashMap<Character, Texture>();
-		dictionary.put('b', new Texture("block.png"));
-			
-		clippingList = new ArrayList<Entity>();
-		nonClippingList = new ArrayList<Entity>();
-	
-		BufferedReader br;
-		String line;
-		int y = 0;
+		String p;
 		try {
-			br = new BufferedReader(new FileReader("stages/" + path));
-			while((line = br.readLine()) != null) {
-				for(int x = 0; x < line.length(); x++)
-				{
-					Character c = line.charAt(x);
-					if(c != ' ')					
-						clippingList.add(new Entity(spriteBatch, dictionary.get(c), x*32, 720-32-y*32));
-				}
-				y++;
-			}
-			br.close();
+			mapFile = String.format("%s.tmx", path);
+			p = String.format("stages/%s", mapFile);
+			
+			map = new TmxMapLoader().load(p);
+			renderer = new OrthogonalTiledMapRenderer(map, 1 / 16f);
+			
+			camera = new OrthographicCamera();
+			camera.setToOrtho(false, 40, 40);
+			camera.update();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -50,13 +45,7 @@ public class Stage extends State {
 	 */
 	@Override
 	public void render() {
-		spriteBatch.begin();
-		for(Entity entity : clippingList) {
-			entity.render();
-		}
-		for(Entity entity : nonClippingList) {
-			entity.render();
-		}
-		spriteBatch.end();
+		renderer.setView(camera);
+		renderer.render();
 	}
 }
--- core/src/com/mygdx/zombies/states/StateManager.java
+++ core/src/com/mygdx/zombies/states/StateManager.java
@@ -5,7 +5,7 @@ public class StateManager {
 	private State currentState;
 	
 	public StateManager() {
-		currentState = new Stage("stage1.txt");
+		currentState = new Stage("teststage");
 	}
 	
 	public void loadState(State newState) {
