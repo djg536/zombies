c8fa6b05860d3f0141b460df309128ea3c7ce5d8
Henry <grayhenryj@gmail.com>
2018-11-19 15:47:20 +0000

    Implemented player movement controls (WASD + mouse)

--- core/src/com/mygdx/zombies/Player.java
+++ core/src/com/mygdx/zombies/Player.java
@@ -1,6 +1,11 @@
 package com.mygdx.zombies;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.lang.Math;
 
 public class Player {
 
@@ -12,13 +17,78 @@ public class Player {
     private int rotation;
     private int positionX;
     private int positionY;
-
-    public Player(Sprite sprite, int x, int y){
-
+    
+    private int mouseX;
+    private int mouseY;
+    private int directionX;
+    private int directionY;
+    private int normalX;
+    private int normalY;
+    private double angle;
+    private float nuAngle;
+    
+    private SpriteBatch spriteBatch;
+    
+    public Player(SpriteBatch spriteBatch, int x, int y){
+    	
+    	this.spriteBatch = spriteBatch;
+    	sprite = new Sprite(new Texture(Gdx.files.internal("block.png")));
+    	positionX = x;
+    	positionY = y;
+    	
     }
-
+    
     private void move(){
-
+    
+    	mouseX = Gdx.input.getX();
+    	mouseY =  -(Gdx.input.getY() - Gdx.graphics.getHeight());	
+    		
+    	directionX = mouseX - positionX;
+    	directionY = mouseY - positionY;
+    	
+    	normalX = 0;
+    	
+    	if(mouseY > positionY) {
+    		normalY = positionY+10;
+    	}
+    	else if(mouseY < positionY) {
+    		normalY = positionY-10;
+    	}
+    	
+    	double dotproduct = ((normalX*directionX)+(normalY*directionY));
+    	double sizeA = (Math.sqrt((normalX*normalX)+(normalY*normalY)));
+    	double sizeB = (Math.sqrt((directionX*directionX)+(directionY*directionY)));
+    	
+    	angle = Math.acos((dotproduct)/((sizeA)*(sizeB)));
+    	  	
+    	angle = Math.toDegrees(angle);
+    	
+    	if(mouseX > positionX) {
+    		angle = -angle;
+    	}
+    	
+    	nuAngle = (float)angle;
+    	  	
+    	if(Gdx.input.isKeyPressed(Keys.W)) {
+        	
+        	positionY+=10;  	
+        } 
+        
+    	if(Gdx.input.isKeyPressed(Keys.S)) {
+    	
+    		positionY-=10;
+    	} 
+    	
+    	if(Gdx.input.isKeyPressed(Keys.A)) {
+        	
+        	positionX-=10;  	
+        } 
+    	
+    	if(Gdx.input.isKeyPressed(Keys.D)) {
+        	
+        	positionX+=10;	
+        } 
+    
     }
 
     private void attack(){
@@ -26,11 +96,17 @@ public class Player {
     }
 
     public void update(){
-
+    	
+    	move();
+    	
     }
 
     public void render(){
-
+    	  	
+    	sprite.setPosition(positionX, positionY);	
+    	sprite.setRotation(nuAngle);
+    	sprite.draw(spriteBatch);
+    	
     }
 
     public int getPositionX(){
