a54ad355938e9c2e22ade3bafc2b16cfddbb3620
Rafee Jenkins <rafeejenkins@gmail.com>
2018-11-29 11:17:51 +0000

    Readded tileset.tsx

---
+++ core/assets/stages/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="RPGpack_sheet" tilewidth="16" tileheight="16" tilecount="4160" columns="80">
+ <image source="../../../../../../Downloads/KenneyRPGpack/Spritesheet/RPGpack_sheet.png" width="1280" height="832"/>
+</tileset>
