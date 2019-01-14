8a85a39789c8038c2e0b316bbb3b785a989504c2
Rafee Jenkins <rafeejenkins@gmail.com>
2019-01-14 20:06:54 +0000

    Adding stages for game
    Assets folder contains source image as well as TSX files. -- DONT MESS WITH IT

---
+++ core/assets/stages/Assets/Alt_Colour.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Alt_Colour" tilewidth="32" tileheight="32" tilecount="1040" columns="40">
+ <image source="Spritesheet/RPGpack_sheet_Recolour.png" width="1280" height="832"/>
+</tileset>
